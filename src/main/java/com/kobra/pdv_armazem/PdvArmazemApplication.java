05a0c2f7cc5b42ad6bc7b4f2707a791e2e9762f0
KOBRA <joaogabrielsh@gmail.com>
2026-06-21 22:59:42 -0300

    first commit

---
+++ src/main/java/com/kobra/pdv_armazem/PdvArmazemApplication.java
@@ -0,0 +1,13 @@
+package com.kobra.pdv_armazem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvArmazemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvArmazemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kobra/pdv_armazem/PdvArmazemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kobra.pdv_armazem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvArmazemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
